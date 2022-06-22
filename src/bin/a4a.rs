514b97a3138b89bf5f634037a3dd40b877e62859
Tertius Roach <tertius.roach@gmail.com>
2022-06-22 10:25:09 +0200

    The "{:?}" token in println

--- src/bin/a4a.rs
+++ src/bin/a4a.rs
@@ -7,4 +7,15 @@
 // * Use a variable set to either true or false
 // * Use a match expression to determine which message to display
 
-fn main() {}
+fn main() {
+    // * Display "it's true" or "it's false" based on the value of a variable
+
+    // * Use a variable set to either true or false
+    let my_bool = true;
+
+    // * Use a match expression to determine which message to display
+    match my_bool {
+        true => println!("It's true"),
+        false => println!("It's false"),
+    }
+}
