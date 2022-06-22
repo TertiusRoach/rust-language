cacb8c0180de3df9074953ff0c3b47b8615dc11f
Tertius Roach <tertius.roach@gmail.com>
2022-06-22 10:24:03 +0200

    Printed my name in Rust

--- src/bin/a1.rs
+++ src/bin/a1.rs
@@ -8,4 +8,18 @@
 // * Use a function to display your last name
 // * Use the println macro to display messages to the terminal
 
-fn main() {}
+// * Use a function to display your first name
+fn first_name() {
+    println!("Tertius");
+}
+
+// * Use a function to display your last name
+fn last_name() {
+    println!("Roach");
+}
+
+fn main() {
+    first_name();
+    last_name();
+    println!("Hello world!");
+}
