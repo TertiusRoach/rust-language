6c520a6f1a16a8208186f380d80c06e517d57996
Tertius Roach <tertius.roach@gmail.com>
2022-06-23 09:22:13 +0200

    Structures in Rust

--- src/bin/a8.rs
+++ src/bin/a8.rs
@@ -5,8 +5,45 @@
 //
 // Notes:
 // * Use an enum to create different flavors of drinks
-// * Use a struct to store drink flavor and fluid ounce information
-// * Use a function to print out the drink flavor and ounces
-// * Use a match expression to print the drink flavor
+enum Flavor {
+    Sparkling,
+    Sweet,
+    Fruity,
+}
 
-fn main() {}
+// * Use a struct to store drink flavor and fluid ounce information
+struct Drink {
+    flavor: Flavor,
+    fluid_oz: f64,
+}
+
+// * Use a function to print out the drink flavor and ounces
+fn print_drink(drink: Drink) {
+    // * Use a match expression to print the drink flavor
+    match drink.flavor {
+        Flavor::Sparkling => println!("Flavor is sparkling"),
+        Flavor::Sweet => println!("Flavor is sweet"),
+        Flavor::Fruity => println!("Flavor is fruity"),
+    }
+    println!("oz: {:?}", drink.fluid_oz);
+}
+
+fn main() {
+    let sweet = Drink {
+        flavor: Flavor::Sweet,
+        fluid_oz: 6.0,
+    };
+    print_drink(sweet);
+
+    let fruity = Drink {
+        flavor: Flavor::Fruity,
+        fluid_oz: 10.0,
+    };
+    print_drink(fruity);
+
+    let sparkling = Drink {
+        flavor: Flavor::Sparkling,
+        fluid_oz: 8.75,
+    };
+    print_drink(sparkling);
+}
