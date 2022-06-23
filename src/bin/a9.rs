c83bc1b5995f352b2160cc9ded390b599f6f7c45
Tertius Roach <tertius.roach@gmail.com>
2022-06-23 09:53:00 +0200

    Tuples in Rust

--- src/bin/a9.rs
+++ src/bin/a9.rs
@@ -6,7 +6,20 @@
 //
 // Notes:
 // * Use a function that returns a tuple
-// * Destructure the return value into two variables
-// * Use an if..else if..else block to determine what to print
+fn coordinate() -> (i32, i32) {
+    (1, 7)
+}
 
-fn main() {}
+fn main() {
+    // * Destructure the return value into two variables
+    let (x, y) = coordinate();
+
+    // * Use an if..else if..else block to determine what to print
+    if y > 5 {
+        println!(">5");
+    } else if y < 5 {
+        println!("<5");
+    } else {
+        println!("=5");
+    }
+}
