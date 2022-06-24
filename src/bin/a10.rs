0e14039b0bee893a081d94bce364dbb36bf0eed0
Tertius Roach <tertius.roach@gmail.com>
2022-06-24 11:15:14 +0200

    Match Expressions in Rust

--- src/bin/a10.rs
+++ src/bin/a10.rs
@@ -5,11 +5,22 @@
 // * Print "its small" if a variable is <= 100
 //
 // Notes:
-// * Use a boolean variable set to the result of
-//   an if..else expression to store whether the value
+
 //   is > 100 or <= 100
 // * Use a function to print the messages
-// * Use a match expression to determine which message
-//   to print
+fn print_message(gt_100: bool) {
+    // * Use a match expression to determine which message
+    //   to print
+    match gt_100 {
+        true => println!("It's big"),
+        false => println!("It's small"),
+    }
+}
 
-fn main() {}
+fn main() {
+    // * Use a boolean variable set to the result of
+    //   an if..else expression to store whether the value
+    let value = 100;
+    let is_gt_100 = value > 100;
+    print_message(is_gt_100);
+}
