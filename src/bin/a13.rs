a2f95a6cd991e950dcdd74303c76470228a3a260
Tertius Roach <tertius.roach@gmail.com>
2022-06-28 09:22:03 +0200

    Vectors in Rust

--- src/bin/a13.rs
+++ src/bin/a13.rs
@@ -5,9 +5,18 @@
 // * Print the total number of elements in a vector
 //
 // Notes:
-// * Use a vector to store 4 numbers
-// * Iterate through the vector using a for..in loop
-// * Determine whether to print the number or print "thirty" inside the loop
-// * Use the .len() function to print the number of elements in a vector
 
-fn main() {}
+fn main() {
+    // * Use a vector to store 4 numbers
+    let my_numbers = vec![10, 20, 30, 40];
+    // * Iterate through the vector using a for..in loop
+    for num in &my_numbers {
+        // * Determine whether to print the number or print "thirty" inside the loop
+        match num {
+            30 => println!("Thirty"),
+            _ => println!("{:?}", num),
+        }
+    }
+    // * Use the .len() function to print the number of elements in a vector
+    println!("Number of elements = {:?}", my_numbers.len());
+}
