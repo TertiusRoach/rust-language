36ca13dc55e78e7dae9ca0a4a3fc4d4914f13869
Tertius Roach <tertius.roach@gmail.com>
2022-07-05 11:53:08 +0200

    Working with Data: Option

--- src/bin/a16.rs
+++ src/bin/a16.rs
@@ -1,14 +1,22 @@
 // Topic: Option
-//
-// Requirements:
+
+struct Student {
+    // * Use a struct containing the student's name and locker assignment
+    // * The locker assignment should use an Option<i32>
+    name: String,
+    locker: Option<i32>,
+}
+
 // * Print out the details of a student's locker assignment
 // * Lockers use numbers and are optional for students
-//
-// Notes:
-// * Use a struct containing the student's name and locker assignment
-// * The locker assignment should use an Option<i32>
-
-// * Use a struct containing the student's name and locker assignment
-// * The locker assignment should use an Option<i32>
-
-fn main() {}
+fn main() {
+    let mary = Student {
+        name: "Mary".to_owned(),
+        locker: Some(3),
+    };
+    println!("Student: {:?}", mary.name);
+    match mary.locker {
+        Some(num) => println!("Locker number: {:?}", num),
+        None => println!("No locker assigned"),
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,24 @@
-enum Discount {
-    Percent(i32),
-    Flat(i32),
-}
-struct Ticket {
-    event: String,
-    price: i32,
+struct Survey {
+    q1: Option<i32>,
+    q2: Option<bool>,
+    q3: Option<String>,
 }
 fn main() {
-    let n = 3;
-    match n {
-        3 => println!("Three"),
-        other => println!("Number: {:?}", other),
-    }
-
-    let flat = Discount::Flat(2);
-    match flat {
-        Discount::Flat(2) => println!("Flat 2"),
-        Discount::Flat(amount) => println!("Flat discount of {:?}", amount),
-        _ => (),
-    }
-
-    let concert = Ticket {
-        event: "Concert".to_owned(),
-        price: 50,
+    let response = Survey {
+        q1: Some(12),
+        q2: Some(true),
+        q3: Some("A".to_owned()),
     };
-    match concert {
-        Ticket { price: 50, event } => println!("Event @ 50 = {:?}", event),
-        Ticket { price, .. } => println!("Price = {:?}", price),
+    match response.q1 {
+        Some(answer) => println!("q1: {:?}", answer),
+        None => println!("q1: no response"),
+    }
+    match response.q2 {
+        Some(answer) => println!("q2: {:?}", answer),
+        None => println!("q2: no response"),
+    }
+    match response.q3 {
+        Some(answer) => println!("q3: {:?}", answer),
+        None => println!("q3: no response"),
     }
 }
